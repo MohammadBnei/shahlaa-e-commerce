0a92d608152dc2b7718baf3410c464842fd6bdbd
MohammadBnei <mohammadamine.banaei@pm.me>
2025-01-17 20:35:48 +0100

    revert medusa config

--- store/medusa-config.ts
+++ store/medusa-config.ts
@@ -1,16 +1,27 @@
-import { loadEnv, defineConfig } from '@medusajs/framework/utils'
+import { loadEnv, defineConfig } from "@medusajs/framework/utils";
 
-loadEnv(process.env.NODE_ENV || 'development', process.cwd())
+loadEnv(process.env.NODE_ENV || "development", process.cwd());
 
 module.exports = defineConfig({
+  admin: {
+    disable: process.env.ADMIN_ENABLED !== "true",
+    backendUrl: process.env.BACKEND_URL,
+    storefrontUrl: process.env.FRONTEND_URL,
+    path: "/",
+  },
   projectConfig: {
     databaseUrl: process.env.DATABASE_URL,
+    redisUrl: process.env.REDIS_URL,
     http: {
       storeCors: process.env.STORE_CORS!,
       adminCors: process.env.ADMIN_CORS!,
       authCors: process.env.AUTH_CORS!,
       jwtSecret: process.env.JWT_SECRET || "supersecret",
       cookieSecret: process.env.COOKIE_SECRET || "supersecret",
-    }
-  }
-})
+    },
+    workerMode: process.env.MEDUSA_WORKER_MODE as
+      | "shared"
+      | "worker"
+      | "server",
+  },
+});
