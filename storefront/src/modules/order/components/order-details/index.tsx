fc473dd7d9982b4689da8caecaee1a88fb9ca707
MohammadBnei <mohammadamine.banaei@pm.me>
2025-01-18 15:51:51 +0100

    fix(orders): displaying order informations

--- storefront/src/modules/order/components/order-details/index.tsx
+++ storefront/src/modules/order/components/order-details/index.tsx
@@ -6,7 +6,7 @@ type OrderDetailsProps = {
   showStatus?: boolean
 }
 
-const OrderDetails = ({ order, showStatus }: OrderDetailsProps) => {
+const OrderDetails = ({ order, showStatus = true }: OrderDetailsProps) => {
   const formatStatus = (str: string) => {
     const formatted = str.split("_").join(" ")
 
@@ -40,18 +40,14 @@ const OrderDetails = ({ order, showStatus }: OrderDetailsProps) => {
           <>
             <Text>
               Order status:{" "}
-              <span className="text-ui-fg-subtle " data-testid="order-status">
-                {/* TODO: Check where the statuses should come from */}
-                {/* {formatStatus(order.fulfillment_status)} */}
+              <span className="text-ui-fg-subtle" data-testid="order-status">
+                {formatStatus(order.fulfillment_status)}
               </span>
             </Text>
             <Text>
               Payment status:{" "}
-              <span
-                className="text-ui-fg-subtle "
-                sata-testid="order-payment-status"
-              >
-                {/* {formatStatus(order.payment_status)} */}
+              <span className="text-ui-fg-subtle" data-testid="payment-status">
+                {formatStatus(order.payment_status)}
               </span>
             </Text>
           </>
