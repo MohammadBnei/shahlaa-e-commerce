0504b607f5a1c097fc934156147fa810032a5f0a
MohammadBnei <mohammadamine.banaei@pm.me>
2025-01-18 14:13:03 +0100

    fix(home): using shahla image for home

--- storefront/src/modules/home/components/hero/index.tsx
+++ storefront/src/modules/home/components/hero/index.tsx
@@ -1,33 +1,28 @@
-import { Github } from "@medusajs/icons"
-import { Button, Heading } from "@medusajs/ui"
+"use client"
+
+import Image from "next/image"
 
 const Hero = () => {
   return (
-    <div className="h-[75vh] w-full border-b border-ui-border-base relative bg-ui-bg-subtle">
-      <div className="absolute inset-0 z-10 flex flex-col justify-center items-center text-center small:p-32 gap-6">
-        <span>
-          <Heading
-            level="h1"
-            className="text-3xl leading-10 text-ui-fg-base font-normal"
-          >
-            Ecommerce Starter Template
-          </Heading>
-          <Heading
-            level="h2"
-            className="text-3xl leading-10 text-ui-fg-subtle font-normal"
-          >
-            Powered by Medusa and Next.js
-          </Heading>
-        </span>
-        <a
-          href="https://github.com/medusajs/nextjs-starter-medusa"
-          target="_blank"
-        >
-          <Button variant="secondary">
-            View on GitHub
-            <Github />
-          </Button>
-        </a>
+    <div className="w-full relative border-b border-ui-border-base">
+      <Image
+        src="/content/home.jpg"
+        alt="ChezScheyda Hero"
+        width={2000}
+        height={1333}
+        className="w-full h-auto md:hidden"
+        priority
+        quality={90}
+      />
+      <div className="hidden md:block h-[calc(100vh-4rem)]">
+        <Image
+          src="/content/home.jpg"
+          alt="ChezScheyda Hero"
+          fill
+          className="object-fill"
+          priority
+          quality={90}
+        />
       </div>
     </div>
   )
